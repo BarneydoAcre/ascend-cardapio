4c0ccbbd465be1d9c87e7d0c7cd40be3bb94eee1
unknown <gabrielfauth@outlook.com>
2022-03-27 21:04:31 -0400

    v1.00.002

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 site para acessar: http://ascend-pedidos.herokuapp.com
+
+*SITE PROJETADO PARA RECEBIMENTO DE PEDIDOS POR WHATSAPP*
+
+- projeto inicialmente sendo desenvolvimento, futuramente será comercializado.
+
+
+*VERSION:*
+- v1.00.002
+- ALTERAÇÃO* Botão adicionar que se localizava no preço do item agora corresponde ao card por inteiro
+- CORREÇÃO* Era necessário clicar duas vezes para gerar o pedido. 
+
+- v1.00.001
+- NOVO* Formas de pagamento adicionadas
+- ERRO* Tela infinita na finalização do pedido corrigida
+- Melhorias Gerais*
--- app/models.py
+++ app/models.py
@@ -1,15 +1,14 @@
-from tabnanny import verbose
 from django.db import models
 
 class Prato(models.Model):
-    name = models.CharField(max_length = 30)
-    desc = models.CharField(max_length = 60)
-    ingredients = models.CharField(max_length = 100)
-    value = models.FloatField()
-    disponibilidade = models.BooleanField(default=True)
+    name = models.CharField(verbose_name="Nome", max_length = 30)
+    desc = models.CharField(verbose_name="Descrição", max_length = 60)
+    ingredients = models.CharField(verbose_name="Ingredientes", max_length = 100)
+    value = models.FloatField(verbose_name="Valor")
+    disponibilidade = models.BooleanField(verbose_name="Disponível", default=True)
 
     # image = models.FileField(upload_to='app/static/public/cardapio/', blank=True)
-    image = models.TextField(max_length=255, blank=True)
+    image = models.TextField(verbose_name="Imagem", max_length=255, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -27,8 +26,13 @@ class Pedido(models.Model):
 class Gerencial(models.Model):
     abertura = models.IntegerField(verbose_name='Abertura', default=18)
     fechamento = models.IntegerField(verbose_name='Fechamento', default=22)
-    logo = models.TextField(blank=True)
-    main_image = models.TextField(blank=True)
+
+    logo = models.TextField(verbose_name="Icone Logo", blank=True)
+    nome_estabelecimento = models.CharField(verbose_name="Nome do Estabelecimento", max_length=12, blank=False)
+    main_image = models.TextField(verbose_name="Imagem Principal", blank=True)
+
+    link_instagram = models.TextField(verbose_name="Link Instagram", blank=True)
+    link_whatsapp = models.TextField(verbose_name="Link WhatsApp", blank=True)
 
     class Meta:
         verbose_name_plural = 'Gerencial'
