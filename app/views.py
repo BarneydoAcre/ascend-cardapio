ad6464a25de32d28ffaeccff091d8a289acb6580
unknown <gabrielfauth@outlook.com>
2022-04-08 12:15:59 -0400

    teste

--- app/views.py
+++ app/views.py
@@ -23,7 +23,8 @@ def index(request):
     
         
     db['produtos'] = models.Produto.objects.all()
-    return render(request, 'home/index.html', db)
+    return render(request, 'test.html', db)
+    # return render(request, 'home/index.html', db)
 
 def promocao(request):
     return render(request, 'home/promocao.html')
