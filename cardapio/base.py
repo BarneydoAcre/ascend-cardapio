1a0fd603127f06f0347f12af711540abcc153d64
unknown <gabrielfauth@outlook.com>
2022-04-09 14:33:42 -0400

    teste

--- cardapio/base.py
+++ cardapio/base.py
@@ -104,6 +104,16 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dc7j420kov8043',
+        'USER': 'xgfkilbokpurvw',
+        'PASSWORD': '4b0fac5998f69160a22d8cf7a463de6759ae75d2fe2d20793d97ad42c884049e',
+        'HOST': 'ec2-52-1-115-6.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
